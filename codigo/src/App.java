7e6c761c20a69eb55c2a3a28ee2ef0700626c19a
Joao Pedro Mairinque <jpmairinque@gmail.com>
2022-09-08 20:18:15 -0300

    rearrange app

--- codigo/src/App.java
+++ codigo/src/App.java
@@ -5,9 +5,9 @@ public class App {
     public static void main(String[] args) throws Exception {
     AgendaCompromisso minhaAgenda = new AgendaCompromisso();
 
-    minhaAgenda.adicionarCompromisso("11/11/2022", "teste", 5, 5);
-    minhaAgenda.adicionarCompromisso("16/11/2022", "teste2", 0, 0);
-    minhaAgenda.adicionarCompromisso("03/11/2022", "teste3", 0, 0);
+    minhaAgenda.adicionarCompromisso("11/11/2022", "Levar cachorro pra passear", 3, 5);
+    minhaAgenda.adicionarCompromisso("10/11/2022", "Reuniao empresa", 1, 30);
+    minhaAgenda.adicionarCompromisso("03/11/2022", "Corrida", 0, 0);
     
     String teste = minhaAgenda.gerarRelatorio();
 
