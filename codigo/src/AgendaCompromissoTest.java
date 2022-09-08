ce92a843cc1bfd1d2790457961aa72fcaddf5fa6
Joao Pedro Mairinque <jpmairinque@gmail.com>
2022-09-08 20:48:25 -0300

    adicionar before each

--- codigo/src/AgendaCompromissoTest.java
+++ codigo/src/AgendaCompromissoTest.java
@@ -2,14 +2,22 @@ package src;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Date;
-import org.junit.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 
 public class AgendaCompromissoTest {
 
+    private static AgendaCompromisso agenda;
+
+    @BeforeEach
+    public void setup() {
+      agenda = new AgendaCompromisso();
+    }
+
     @Test
     public void testaAdicionaCompromisso(){
-        AgendaCompromisso agenda = new AgendaCompromisso();
         agenda.adicionarCompromisso("10/10/2022", "Jogar bola", 5, 1);
     
         Compromisso compromisso = agenda.getDatas().get(0).getCompromissos().get(0);
@@ -18,25 +26,23 @@ public class AgendaCompromissoTest {
 
     @Test
     public void testaRepeticaoCompromisso(){
-        AgendaCompromisso agenda = new AgendaCompromisso();
         agenda.adicionarCompromisso("05/05/2022", "Médico", 5, 3);
         
         
         // teste dia 10/05
         Date data = agenda.converterStringParaDate("10/05/2022");
-        Compromisso compromisso = agenda.acharEmDatas(data).getCompromissos().get(0);
-        assertEquals(compromisso.dadosCompromisso(), "\nMédico\n");
+        Date dataCompromisso = agenda.acharEmDatas(data).getCompromissos().get(0).getDataCompromisso();
+        assertEquals(data, dataCompromisso);
 
         // teste dia 15/05
         data = agenda.converterStringParaDate("15/05/2022");
-        compromisso = agenda.acharEmDatas(data).getCompromissos().get(0);
-        assertEquals(compromisso.dadosCompromisso(), "\nMédico\n");
+        dataCompromisso = agenda.acharEmDatas(data).getCompromissos().get(0).getDataCompromisso();
+        assertEquals(data, dataCompromisso);
 
         // teste dia 20/05
         data = agenda.converterStringParaDate("20/05/2022");
-        compromisso = agenda.acharEmDatas(data).getCompromissos().get(0);
-        assertEquals(compromisso.dadosCompromisso(), "\nMédico\n");
-
+        dataCompromisso = agenda.acharEmDatas(data).getCompromissos().get(0).getDataCompromisso();
+        assertEquals(data, dataCompromisso);
 
     }
 
